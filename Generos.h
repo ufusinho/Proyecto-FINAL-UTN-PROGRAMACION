794fec7cec741c59f7de1cedbe80f4a2070d8734
aguersinho <totoaguer1@gmail.com>
2026-06-15 19:22:13 -0300

    Add files via upload

---
+++ Generos.h
@@ -0,0 +1,27 @@
+#ifndef GENEROS_H_INCLUDED
+#define GENEROS_H_INCLUDED
+
+#define ARCHIVO_GENEROS "generos.dat"
+#define MAX_GENEROS 20
+#define ACTIVO 1
+#define INACTIVO 0
+
+typedef struct{
+ int idGenero;
+ char nombre[50];
+ char descripcion[150];
+ int activo;
+}stGenero;
+
+int cargarGen(stGenero lista[], int cantidad);
+int guardarGen(stGenero lista[],int cantidad);
+void altaGen(stGenero lista[],int cantidad);
+void bajaGen(stGenero lista[],int cantidad,int cantContenido,int idGenContenido[]);
+void modificarGen(stGenero lista[],int cantidad);
+void consultarGen(stGenero lista[],int cantidad);
+void listarGen(stGenero lista[],int cantidad);
+int buscarGenXid(stGenero lista[], int cantidad, int id);
+int buscarGenXNombre(stGenero lista[], int cantidad, const char nombre[]);
+int generarIDGEN(stGenero lista[],int cantidad);
+
+#endif
