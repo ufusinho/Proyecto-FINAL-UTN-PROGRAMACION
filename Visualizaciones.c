913dc98a80ba52674cc9f9b7217e01932d55394c
aguersinho <totoaguer1@gmail.com>
2026-06-19 11:12:48 -0300

    Visualizaciones.c

--- Visualizaciones.c
+++ Visualizaciones.c
@@ -1,14 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h>
-#include <conio.h>
+
 #include "Visualizaciones.h"
 
-void limpiarBufferVis(void)
+void limpiarBufferBis(void)
 {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+void menuVisualizaciones(stVisualizacion lista[], int *cantidad,int idUsuarioLogueado,int esAdmin,int *idsContenidosActivos,int totalActivos)
+{
+    int opcion;
+    do{
+        puts("\n\n===== MENU VISUALIZACIONES =====");
+        if(esAdmin){
+            printf("\n 1. Listar visualizaciones");
+            printf("\n 2. Dar de baja visualizacion");
+            printf("\n 0. Volver");
+        }else{
+            printf("\n 1. Registrar visualizacion");
+            printf("\n 2. Listar mis visualizaciones");
+            printf("\n 3. Eliminar una visualizacion");
+            printf("\n 0. Volver");
+        }
+        printf("\n\n Opcion: ");
+        scanf("%d", &opcion);
+        limpiarBufferBis();
+        if(esAdmin){
+
+            switch(opcion){
+                case 1:
+                    listarVisualizaciones(lista,*cantidad,idUsuarioLogueado,esAdmin);
+                    break;
+                case 2:
+                    bajaVisualizacion(lista,*cantidad,idUsuarioLogueado,esAdmin);
+                    break;
+                case 0:
+                    printf("\n Volviendo...");
+                    break;
+                default:
+                    printf("\nOpcion invalida.");
+            }
+        }else{
+
+            switch(opcion){
+
+                case 1:
+                    altaVisualizacion(lista,cantidad,idUsuarioLogueado,idsContenidosActivos,totalActivos);
+                    break;
+                case 2:
+                    listarVisualizaciones(lista,*cantidad,idUsuarioLogueado,esAdmin);
+                    break;
+                case 3:
+                    bajaVisualizacion(lista,*cantidad,idUsuarioLogueado,esAdmin);
+                    break;
+                case 0:
+                    printf("\n Volviendo...");
+                    break;
+                default:
+                    printf("\nOpcion invalida.");
+            }
+        }
+
+    }while(opcion != 0);
 }
 ///CARGAR Y GUARDAR///
 int cargarVisualizaciones(stVisualizacion lista[], int *cantidad)
@@ -95,7 +150,7 @@ void cargarFecha(int *dia, int *mes, int *anio)
         printf("\n Dia  : "); scanf("%d", dia);
         printf("\n Mes  : "); scanf("%d", mes);
         printf("\n Anio : "); scanf("%d", anio);
-        limpiarBufferVis();
+        limpiarBufferBis();
 
         if(!validarFecha(*dia, *mes, *anio)){
             printf("\n Fecha invalida. Intente nuevamente.");
@@ -109,7 +164,7 @@ int cargarCalificacion(void)
     do{
         printf("\n Calificacion (1 al 10): ");
         scanf("%d", &cal);
-        limpiarBufferVis();
+        limpiarBufferBis();
 
         if(cal < 1 || cal > 10){
             printf("\n Calificacion invalida.");
@@ -144,7 +199,7 @@ void altaVisualizacion(stVisualizacion lista[], int *cantidad,int idUsuarioLogue
     do{
         printf("\n\n Ingrese el ID del contenido visto: ");
         scanf("%d", &idContenido);
-        limpiarBufferVis();
+        limpiarBufferBis();
 
         esValido = 0;
         for(int i = 0; i < totalActivos; i++){
@@ -199,7 +254,7 @@ void bajaVisualizacion(stVisualizacion lista[], int cantidad,int idUsuarioLoguea
 
     printf("\n Ingrese el ID de la visualizacion a eliminar: ");
     scanf("%d", &idBuscar);
-    limpiarBufferVis();
+    limpiarBufferBis();
 
     for(int i = 0; i < cantidad; i++){
         if(lista[i].idVisualizacion == idBuscar){
@@ -233,7 +288,7 @@ void bajaVisualizacion(stVisualizacion lista[], int cantidad,int idUsuarioLoguea
     char confirmacion;
     printf("\n\n Confirma la baja? (s/n): ");
     scanf(" %c", &confirmacion);
-    limpiarBufferVis();
+    limpiarBufferBis();
 
     if(confirmacion == 's' || confirmacion == 'S'){
         lista[pos].activo = 0;
@@ -272,258 +327,4 @@ void listarVisualizaciones(stVisualizacion lista[], int cantidad,int idUsuarioLo
     }else{
         printf("\n Total: %d", total);
     }
-}
-///BUSQUEDA///
-void buscarContenidoVistoPorUsuario(stVisualizacion lista[], int cantidad,int *idsUsuarios, char nombresUsuarios[][40],int totalUsuarios,int *idsContenidos, char titulos[][60],int totalContenidos)
-{
-
-    int idBuscar = 0;
-    int u, i, c;
-    int encontrado = 0;
-    int total      = 0;
-
-    printf("\n Usuarios registrados:");
-    for(u = 0; u < totalUsuarios; u++){
-        printf("\n [%d] %s", idsUsuarios[u], nombresUsuarios[u]);
-    }
-
-    printf("\n\n Ingrese el ID del usuario a consultar: ");
-    scanf("%d", &idBuscar);
-    limpiarBufferVis();
-
-    for(u = 0; u < totalUsuarios; u++){
-        if(idsUsuarios[u] == idBuscar){
-            encontrado = 1;
-            break;
-        }
-    }
-
-    if(encontrado == 0){
-        printf("\n No se encontro un usuario con ese ID.");
-        return;
-    }
-
-    printf("\n\n ===== CONTENIDO VISTO POR: %s =====", nombresUsuarios[u]);
-
-    for(i = 0; i < cantidad; i++){
-
-        if(lista[i].idUsuario != idBuscar || lista[i].activo != 1) continue;
-
-        char *titulo = "Desconocido";
-        for(c = 0; c < totalContenidos; c++){
-            if(idsContenidos[c] == lista[i].idContenido){
-                titulo = titulos[c];
-                break;
-            }
-        }
-        printf("\n Contenido    : %s", titulo);
-        printf("\n Fecha        : %02d/%02d/%04d", lista[i].fechaDia, lista[i].fechaMes, lista[i].fechaAnio);
-        printf("\n Calificacion : %d", lista[i].calificacion);
-        printf("\n -------------------------------");
-        total++;
-    }
-
-    if(total == 0){
-        printf("\n El usuario no tiene visualizaciones registradas.");
-    }else{
-        printf("\n Total: %d", total);
-    }
-}
-///REPORTES///
-void reportePromedioCalificaciones(stVisualizacion lista[], int cantidad,int *idsContenidos, char titulos[][60],int totalContenidos)
-{
-    printf("\n\n ===== PROMEDIO DE CALIFICACIONES POR CONTENIDO =====");
-
-    for(int c = 0; c < totalContenidos; c++){
-
-        int suma   = 0;
-        int vistos = 0;
-
-        for(int i = 0; i < cantidad; i++){
-            if(lista[i].activo == 1 && lista[i].idContenido == idsContenidos[c]){
-                suma += lista[i].calificacion;
-                vistos++;
-            }
-        }
-
-        if(vistos > 0){
-            printf("\n %-40s Promedio: %.2f (%d vistas)", titulos[c], (float)suma / vistos, vistos);
-        }else{
-            printf("\n %-40s Sin visualizaciones", titulos[c]);
-        }
-    }
-}
-void reporteTop5Contenidos(stVisualizacion lista[], int cantidad,int *idsContenidos, char titulos[][60],int totalContenidos)
-{
-    int   idsAux[MAX_VISUALIZACIONES];
-    char  titulosAux[MAX_VISUALIZACIONES][60];
-    float promedios[MAX_VISUALIZACIONES];
-    int   vistos[MAX_VISUALIZACIONES];
-    int   total = 0;
-
-    for(int c = 0; c < totalContenidos; c++){
-
-        int suma = 0;
-        int cant = 0;
-
-        for(int i = 0; i < cantidad; i++){
-            if(lista[i].activo == 1 && lista[i].idContenido == idsContenidos[c]){
-                suma += lista[i].calificacion;
-                cant++;
-            }
-        }
-
-        if(cant > 0){
-            idsAux[total] = idsContenidos[c];
-            strcpy(titulosAux[total], titulos[c]);
-            promedios[total] = (float)suma / cant;
-            vistos[total] = cant;
-            total++;
-        }
-    }
-    for(int i = 0; i < total - 1; i++){
-        for(int j = 0; j < total - i - 1; j++){
-            if(promedios[j] < promedios[j+1]){
-
-                float pAux = promedios[j];
-                promedios[j] = promedios[j+1];
-                promedios[j+1] = pAux;
-
-                int vAux = vistos[j];
-                vistos[j] = vistos[j+1];
-                vistos[j+1] = vAux;
-
-                int idAux = idsAux[j];
-                idsAux[j] = idsAux[j+1];
-                idsAux[j+1] = idAux;
-
-                char tituloAux[60];
-                strcpy(tituloAux, titulosAux[j]);
-                strcpy(titulosAux[j], titulosAux[j+1]);
-                strcpy(titulosAux[j+1], tituloAux);
-            }
-        }
-    }
-
-    printf("\n\n ===== TOP 5 CONTENIDOS POR CALIFICACION =====");
-
-    if(total == 0){
-        printf("\n No hay contenidos con visualizaciones registradas.");
-    }else{
-        int limite = (total < 5) ? total : 5;
-        for(int i = 0; i < limite; i++){
-            printf("\n %d. %-40s Promedio: %.2f (%d vistas)", i + 1, titulosAux[i], promedios[i], vistos[i]);
-        }
-    }
-
-}
-void reporteUsuarioMasVisualizaciones(stVisualizacion lista[], int cantidad,int *idsUsuarios, char nombresUsuarios[][40],int totalUsuarios)
-{
-    int maxCant = 0;
-    int maxIdx  = -1;
-
-    for(int u = 0; u < totalUsuarios; u++){
-
-        int contador = 0;
-
-        for(int i = 0; i < cantidad; i++){
-            if(lista[i].activo == 1 && lista[i].idUsuario == idsUsuarios[u]){
-                contador++;
-            }
-        }
-        if(contador > maxCant){
-            maxCant = contador;
-            maxIdx  = u;
-        }
-    }
-
-    printf("\n\n ===== USUARIO CON MAS VISUALIZACIONES =====");
-
-    if(maxIdx == -1){
-        printf("\n Ningun usuario tiene visualizaciones registradas.");
-    }else{
-        printf("\n Usuario : %s (ID %d)", nombresUsuarios[maxIdx], idsUsuarios[maxIdx]);
-        printf("\n Total   : %d visualizaciones", maxCant);
-    }
-
-}
-void reporteContenidosSinVisualizaciones(stVisualizacion lista[], int cantidad,int *idsContenidos, char titulos[][60],int totalContenidos)
-{
-    int sinVistas = 0;
-
-    printf("\n\n ===== CONTENIDOS SIN VISUALIZACIONES =====");
-
-    for(int c = 0; c < totalContenidos; c++){
-
-        int tieneVistas = 0;
-
-        for(int i = 0; i < cantidad; i++){
-            if(lista[i].activo == 1 && lista[i].idContenido == idsContenidos[c]){
-                tieneVistas = 1;
-                break;
-            }
-        }
-
-        if(tieneVistas == 0){
-            printf("\n [%d] %s", idsContenidos[c], titulos[c]);
-            sinVistas++;
-        }
-    }
-
-    if(sinVistas == 0){
-        printf("\n Todos los contenidos tienen al menos una visualizacion.");
-    }else{
-        printf("\n Total sin visualizaciones: %d", sinVistas);
-    }
-}
-void reporteVisualizacionesUsuario(stVisualizacion lista[], int cantidad,int idUsuario,int *idsContenidos, char titulos[][60],int totalContenidos)
-{
-    int total = 0;
-
-    printf("\n\n ===== MIS VISUALIZACIONES =====");
-
-    for(int i = 0; i < cantidad; i++){
-
-        if(lista[i].idUsuario != idUsuario || lista[i].activo != 1) continue;
-
-        char *titulo = "Desconocido";
-        for(int c = 0; c < totalContenidos; c++){
-            if(idsContenidos[c] == lista[i].idContenido){
-                titulo = titulos[c];
-                break;
-            }
-        }
-
-        printf("\n Contenido    : %s", titulo);
-        printf("\n Fecha        : %02d/%02d/%04d", lista[i].fechaDia, lista[i].fechaMes, lista[i].fechaAnio);
-        printf("\n Calificacion : %d", lista[i].calificacion);
-        printf("\n -------------------------------");
-        total++;
-    }
-
-    if(total == 0){
-        printf("\n No tiene visualizaciones registradas.");
-    }else{
-        printf("\n Total: %d", total);
-    }
-}
-void reportePromedioUsuario(stVisualizacion lista[], int cantidad, int idUsuario)
-{
-    int suma   = 0;
-    int vistos = 0;
-
-    for(int i = 0; i < cantidad; i++){
-        if(lista[i].idUsuario == idUsuario && lista[i].activo == 1){
-            suma += lista[i].calificacion;
-            vistos++;
-        }
-    }
-    printf("\n\n ===== MI PROMEDIO DE CALIFICACIONES =====");
-
-    if(vistos == 0){
-        printf("\n No tiene visualizaciones registradas.");
-    }else{
-        printf("\n Promedio: %.2f (basado en %d visualizaciones)", (float)suma / vistos, vistos);
-    }
-
 }
